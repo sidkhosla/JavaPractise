d9239b4c2fa8b180d5af564ca08f4b84705c5699
sidkhosla <sidkhosla2@gmail.com>
2020-12-26 10:09:38 +0530

    Pushing code from Day 1 and day 2 and day 3 of the project

---
+++ src/Day2/Main.java
@@ -0,0 +1,118 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //----Understanding array and for loops
+//        int s = 10;
+//        int a[] = new int[s];
+//        Scanner sc  = new Scanner(System.in);
+//        for(int x = 0; x < s; x++) {
+//        a[x] = sc.nextInt();
+//        }
+//
+//        System.out.println(a[5]);
+        //Printing 2 arrays combined
+//        int z=5;
+//        int a[] = {1,2,3,4,5};
+//        int b[]= {11,12,13,14,15};
+//        int c[]= new int[10];
+//
+//        int x;
+//        for(x=0; x<5; x++)
+//        {
+//            c[x]=a[x];
+//            System.out.println(c[x]);
+//        }
+//        for(x=0;x<5;x++)
+//        {
+//            c[z]=b[x];
+//            System.out.println(c[z]);
+//            z++;
+//        }
+//---------
+//        int z = 0;
+//        int x;
+//        for(x = 1; x < 10; x++) {
+//            System.out.println(x);
+//            if(x < 5)
+//            {
+//                z = z + x;
+//            }
+//        }
+//        System.out.println(z);
+//---------
+
+//        int b = 0;
+//        int x;
+//        for(x = 1; x<100; x++){
+//            b++;
+//            if(b!=5){
+//                System.out.println(x);
+//            }
+//            else{
+//                b = 0;
+//            }
+//        }
+//        ------------------
+//        int c = 0;
+//        int d = 1;
+//        int x;
+//        for(x=1; x<=10; x++){
+//            c++;
+//            if(c<=d){
+//                System.out.print(x);
+//            }
+//            if(c==d){
+//                c=0;
+//                d=d+1;
+//                System.out.print("\n");
+//            }
+//        }
+        
+//        int c = 4;
+//        int d = 0;
+//        int x;
+//        for(x = 1; x<=10; x++){
+//            d++;
+//            System.out.print(x);
+//            if(d==c){
+//                c--;
+//                d=0;
+//                System.out.print("\n");
+//            }
+//        }
+
+//        int b = 10;
+//        for(int x = 1; x<=15; x++){
+//            if(x<=b){
+//                System.out.println(x);
+//                x=b;
+//            }
+//            else{
+//                System.out.println(x);
+//            }
+//        }
+
+
+//        int a;
+//        int b = 0;
+//        for(int x = 1; x <=100; x++){
+//            System.out.println(x);
+//            if(x == 10){
+//                Scanner sc = new Scanner(System.in);
+//                b = sc.nextInt();
+//
+//
+//            }
+//        }
+
+
+
+
+
+        }
+
+
+    }
---
+++ src/com/spartaglobal/sidhant/Day1/Main.java
@@ -0,0 +1,101 @@
+package com.spartaglobal.sidhant.Day1;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Random rd = new Random();
+//        int a = rd.nextInt(100);
+//        int c = rd.hashCode();
+//        int b = rd.nextInt(10000);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(args[0]);
+//
+//    }
+//
+//    public static void string(String[] args) {
+//        System.out.println(args[0]);
+//int a = 10;
+//int b = ++a;
+//        System.out.println(a);
+//        System.out.println(b);
+//        int a = 10;
+//                int b = 20;
+//                int c = a++ + b++;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        int a =15;
+//        int b = 30;
+//        int c = ++b +a+b++;//31+15+31
+//        System.out.println(c);
+//        System.out.println(b);
+
+//        int a = 15;
+//        int b = 50;
+//        int c = b++ +b++ + ++a;//50+51+16
+//        System.out.println(a);//16
+//        System.out.println(b);//52
+//        System.out.println(c);//117
+
+//        int a = 15;
+//        int b = 50;
+//        a = ++a + b++;
+//        System.out.println(a);
+//        System.out.println(b);
+//        int a = 10;
+//        int b = 15;
+//        System.out.println((a>b)? a:b);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please put an integer: \n");
+//        String a = sc.nextLine();
+//
+//        switch(a)
+//        {
+//            case "1":
+//            System.out.println("ok");
+//            break;
+//
+//            case "2":
+//            System.out.println("ok2");
+//            break;
+//            case "abc":
+//                System.out.println(("This is the one"));
+//        int a = 10;
+//        switch (a)
+//        {
+//            case 1:
+//                System.out.println("ok");
+//            case 10:
+//                System.out.println("bye");
+//                break;
+//            case 11:
+//                System.out.println("ok");
+////        }
+//        int a = 0;
+//        switch(a)
+//        {
+//            default:
+//                System.out.println("Ok done");
+//                break;
+//            case 1:
+//                System.out.println("ore");
+//                break;
+//            case 2:
+//                System.out.println("bye");
+//                break;
+//            case 10:
+//                System.out.println("case 10");
+//                break;
+//        }
+        // String function and its methods
+        // .split in strings
+        }
+
+    }
