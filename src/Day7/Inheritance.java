e365ccaf7aaaa08c438f544c74e0b88909176143
sidkhosla <sidkhosla2@gmail.com>
2021-01-01 13:28:32 +0530

    Uploading day8 plus updated day 7 mini project

--- src/Day7/Inheritance.java
+++ src/Day7/Inheritance.java
@@ -10,6 +10,7 @@ class inheritance {
    public  String Name;
    public String cls;
    public String rollno;
+//--Small project to learn inheritance
   inheritance(String nm,String cl,String rll){
     setName(nm);
     setCls(cl);
@@ -40,13 +41,11 @@ class inheritance {
     }
 }
 class gettingValues {
-    static int n;
     static inheritance[] in = null;
     static Scanner sc = new Scanner(System.in);
     static Scanner scstr = new Scanner(System.in);
 
     public static void main(String[] args) {
-
 
         System.out.println("please enter how many entries do you want to store ");
         int n = sc.nextInt();
@@ -62,6 +61,8 @@ class gettingValues {
             in[i] = new inheritance(nam, cls, rollNo);
             // System.out.println(in.getName()+" "+in.getRollno()+" "+in.getCls());
         }
+
+
         for (int i = 0; i < n; i++) {
             try {
                 FileWriter myWriter = new FileWriter("File.txt", true);
@@ -76,7 +77,8 @@ class gettingValues {
 //-------Switch
         System.out.println("Enter the number for the option you want to execute\n" +
                 "1. Read the file\n" +
-                "2. Search by roll number\n");
+                "2. Search by roll number\n" +
+                "3. Search and change by roll number\n");
         int option = sc.nextInt();
         switch (option) {
             case 1:
@@ -84,6 +86,9 @@ class gettingValues {
                 break;
             case 2:
                 search(n);
+                break;
+            case 3:
+                searchAndChange(n);
                 break;
             default:
                 // code block
@@ -112,8 +117,6 @@ class gettingValues {
 
     //Search
     public static void search(int n) {
-        System.out.println(in.length);
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the roll number you want to search for: ");
         String s = scstr.nextLine();
         int b = 0;
@@ -124,14 +127,66 @@ class gettingValues {
             }
 
         }
-        if(b==0){
+        if (b == 0) {
             System.out.println("No Match");
 
         }
+
+    }
+
+    //---Search and edit.
+    public static void searchAndChange(int t) {
+        System.out.println("Enter the roll number you want to change the record for: ");
+        String s = scstr.nextLine();
+
+        for (int i = 0; i < in.length; i++) {
+            if (s.equals(in[i].getRollno())) {
+                System.out.println("Press 1 to update name or 2 to update class\n");
+                String upNameOrClass = scstr.nextLine();
+                switch (upNameOrClass) {
+                    case "1":
+                        System.out.println("Enter new name: ");
+                        String setNewName = scstr.nextLine();
+                        in[i].setName(setNewName);
+                        break;
+                    case "2":
+                        System.out.println("Enter new class: ");
+                        String setNewClass = scstr.nextLine();
+                        in[i].setCls(setNewClass);
+                        break;
+
+                    // code block
+                }
+            }
+//                System.out.println(in[i].getName() + " " + in[i].getCls());
+
+        }
+        //System.out.println("Do you want to print the change to the file: \n" + "answer in y/n \n");
+        //String answer = scstr.nextLine();
+        //if (answer.equals("y")) {
+        System.out.println(in.length);
+        try{
+            FileWriter myWriter = new FileWriter("File.txt");
+            myWriter.write((""));
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        for (int j = 0; j < in.length; j++) {
+            try {
+                FileWriter myWriter = new FileWriter("File.txt",true);
+                myWriter.write(in[j].getName() + " " + in[j].getCls() + " " + in[j].getRollno() + "\n");
+                myWriter.close();
+//            System.out.println(in[j].getName() + " " + in[j].getCls() + " " + in[j].getRollno());
+
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
     }
 }
-
-
 //-- edit student details by roll number
 //user should be asked what they want to change name or class record of the student with that roll number.
 
