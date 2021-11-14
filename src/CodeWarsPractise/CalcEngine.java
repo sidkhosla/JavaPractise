3e1557d6334b15351f6a585caf32721e0b90951e
sidkhosla <sidkhosla2@gmail.com>
2021-11-14 03:08:48 +0000

    handling string interaction within the calculator the app

--- src/CodeWarsPractise/CalcEngine.java
+++ src/CodeWarsPractise/CalcEngine.java
@@ -1,5 +1,8 @@
 package CodeWarsPractise;
 
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
 public class CalcEngine {
     //mini calculator function for java fundamentals fresher
     public static void main(String[] args) {
@@ -14,10 +17,27 @@ public class CalcEngine {
             }
             for (double currentResult : results)
                 System.out.println(currentResult);
-        } else if(args.length == 3)
+        } else if(args.length == 1 && args[0].equals("interactive"))
+            executeInteractively();
+        else if(args.length == 3)
             handleCommandLine(args);
         else
             System.out.println("Please provide an operation code and 2 numeric values");
+    }
+    static void executeInteractively(){
+        System.out.println("Enter an operation and two numbers");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        performOperation(parts);
+    }
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
     }
 
     private static void handleCommandLine(String[] args) {
@@ -50,5 +70,24 @@ public class CalcEngine {
         }
         return result;
 
+    }
+
+    static char opCodeFromString(String operationName){
+        return operationName.charAt(0);
+    }
+
+    static double valueFromWord(String word){
+        String[] numberWords = {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"
+        };
+        double value = 0d;
+        for (int index = 0; index < numberWords.length; index++){
+            if (word.equals(numberWords[index])) {
+                value = index;
+                break;
+            }
+        }
+        return value;
     }
 }
