5f148817c66e16768b129a0cd2b0c073accca67d
sidkhosla <sidkhosla2@gmail.com>
2021-11-14 02:05:19 +0000

    making parameters passable using terminal/cmd

--- src/CodeWarsPractise/CalcEngine.java
+++ src/CodeWarsPractise/CalcEngine.java
@@ -1,6 +1,6 @@
 package CodeWarsPractise;
 
-public class Main {
+public class CalcEngine {
     //mini calculator function for java fundamentals fresher
     public static void main(String[] args) {
         double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
@@ -8,11 +8,24 @@ public class Main {
         char[] opCodes = {'d', 'a', 's', 'm'};
         double[] results = new double[opCodes.length];
 
-        for (int i = 0; i < opCodes.length; i++) {
-            results[i] = execute(opCodes[i], leftVals[i],rightVals[i] );
-        }
-        for (double currentResult : results)
-            System.out.println(currentResult);
+        if (args.length == 0) {
+            for (int i = 0; i < opCodes.length; i++) {
+                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+            }
+            for (double currentResult : results)
+                System.out.println(currentResult);
+        } else if(args.length == 3)
+            handleCommandLine(args);
+        else
+            System.out.println("Please provide an operation code and 2 numeric values");
+    }
+
+    private static void handleCommandLine(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
     }
 
     static double execute(char opCode, double leftVal, double rightVal) {
