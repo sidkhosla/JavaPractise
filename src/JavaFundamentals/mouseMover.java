394915dbd094174d50f159ba5fc3cc4362c43c56
sidkhosla <sidkhosla2@gmail.com>
2023-07-28 12:46:44 +0100

    Default changes

---
+++ src/JavaFundamentals/mouseMover.java
@@ -0,0 +1,21 @@
+package JavaFundamentals;
+import java.awt.*;
+import java.util.Random;
+
+public class mouseMover {
+    public static final int FIVE_SECONDS = 15000;
+    public static final int MAX_Y = 400;
+    public static final int MAX_X = 400;
+
+    public mouseMover() {
+    }
+    public static void main(String... args) throws Exception {
+        Robot robot = new Robot();
+        Random random = new Random();
+
+        while (true) {
+            robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
+            Thread.sleep(FIVE_SECONDS);
+        }
+    }
+}
